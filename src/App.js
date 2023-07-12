63e5db7eb15aaaa819000a5d1ab2f746b007c937
Abhishek Vijayakumar <abhishekwhizzer@gmail.com>
2023-07-12 18:08:18 -0500

    added pagination implementation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
 
   const fetchProducts = async () => {
     const res = await fetch("https://dummyjson.com/products?limit=80");
@@ -23,14 +24,26 @@ const App = () => {
     <div>
       {products.length > 0 && (
         <div className="products">
-          {products.map((product) => {
+          {products.slice(0, page * 10).map((product) => {
             return (
               <span key={product.id} className="products__single">
                 <img src={product.thumbnail} />
-                <span>{product.title}</span>
+                <span>
+                  {product.id}- {product.title}
+                </span>
               </span>
             );
           })}
+        </div>
+      )}
+
+      {products.length > 0 && (
+        <div className="pagination">
+          <span>▶️</span>
+          {[...Array(products.length / 8)].map((_, i) => {
+            return <span key={i}>{i + 1}</span>;
+          })}
+          <span>◀️</span>
         </div>
       )}
     </div>
